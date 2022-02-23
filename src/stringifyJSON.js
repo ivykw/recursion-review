d56c5e108dece37fd39d513af8a4675426df19eb
Andrew <andrew.orodenker@gmail.com>
2022-02-22 16:48:22 -0800

    stringify almost compelete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = [];
+  if (typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+    return '' + obj;
+  } if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  } if (obj === null) {
+    return 'null';
+  } if (Array.isArray(obj)) {
+    if (obj[0] === undefined) {
+      return '[]';
+    }
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  } if (typeof obj === 'object') {
+    var stringObj = [];
+    for (key in obj) {
+      var stringKey = stringifyJSON(key);
+      var stringVal = stringifyJSON(obj[key]);
+      stringObj.push(stringKey + ':' + stringVal);
+    }
+    return '{' + stringObj.join(',') + '}';
+  }
 };
+
+
+
+//number - 'number' =
+//string
+//boolean - 'true' or 'false' =
+//array - '[' + array + ']'
+//object - '{' + result + '}'
+//function - ignore
+//null - 'null'
+//undefined - ignore
